0499d85d1f15f10e0e0c1f8d31d420b0691a54a2
jeydevops <jeyanthi.be@gmail.com>
2014-11-25 01:25:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app=http.createServer(function (req, res) {
 //creates server
   res.writeHead(200, {'Content-Type': 'text/plain'});
   //sets the right header and status code
-  res.end('Hello World\n');
+  res.end('Hello Viewer!!! Welcome to our Amazon NodeJS DevOps Demo...\n');
   //outputs string with line end symbol
 }).listen(80, "ec2-54-237-64-198.compute-1.amazonaws.com");
 //sets port and IP address of the server
